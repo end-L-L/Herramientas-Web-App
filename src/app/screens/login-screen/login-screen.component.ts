cec2e417fc5b31489eaf0af21bae3e99fb8c0212
Boset <boss-x86@protonmail.com>
2023-09-14 21:24:15 -0600

    terminando el login

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
   styleUrls: ['./login-screen.component.scss']
 })
-export class LoginScreenComponent {
+export class LoginScreenComponent implements OnInit{
   
-  constructor() {}
+  public type: String = "password";
+  public username: String = "";
+  public password: String = "";
+
+  public errors:any = {};
+
+  constructor(
+    private router: Router
+  ) {}
 
   ngOnInit():void{
 
+  }
+
+  public login(){
+    if(this.username == ""){
+      this.errors.username = "Campo requerido";
+    }
+    if(this.password == ""){
+      this.errors.password = "Campo requerido";
+    }
+  }
+
+  public showPassword(){
+    if(this.type == "password"){
+      this.type = "text";
+    }else{
+      this.type = "password";
+    }
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro"]);
   }
 
 }
