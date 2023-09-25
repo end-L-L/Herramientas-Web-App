6f9f716b887e5f41b4a714fef43985425d1d51de
Boset <boss-x86@protonmail.com>
2023-09-25 16:18:42 -0600

    ajustando el registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 //Componentes
 import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
 import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
 import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NuevoLoginScreenComponent } from './screens/nuevo-login-screen/nuevo-login-screen.component';
-//Angular Material
-//import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+
+//Angular Material
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker'; 
 
 @NgModule({
   declarations: [
@@ -29,7 +33,11 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/screens/registro-screen/registro-screen.component.ts
+++ src/app/screens/registro-screen/registro-screen.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-registro-screen',
   templateUrl: './registro-screen.component.html',
   styleUrls: ['./registro-screen.component.scss']
 })
-export class RegistroScreenComponent {
+export class RegistroScreenComponent implements OnInit {
+
+  //Propiedades
+  public editar:boolean = false;
+  public user: any = {};
+
+  constructor(
+    private location: Location,
+  ) { }
+
+  ngOnInit(): void {
+    this.user = this.esquemaUser();
+    console.log("User: ", this.user);
+
+  }
+
+  public regresar(){
+    this.location.back();
+  }
+
+  public esquemaUser(){
+    return {
+      'matricula': '',
+      'first_name': '',
+      'last_name': '',
+      'email': '',
+      'password': '',
+      'confirmar_password': '',
+      'fecha_nacimiento': '',
+      'curp': '',
+      'rfc': '',
+      'edad': '',
+      'telefono': '',
+      'ocupacion': '',
+    }
+  }
 
 }
