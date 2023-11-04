2ce75569efb7da38959e061403b13fb57c9ed960
Boset <boss-x86@protonmail.com>
2023-11-04 16:48:14 -0600

    crud home screen

--- src/app/screens/home-screen/home-screen.component.ts
+++ src/app/screens/home-screen/home-screen.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FacadeService } from 'src/app/services/facade.service';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
   styleUrls: ['./home-screen.component.scss']
 })
-export class HomeScreenComponent {
+export class HomeScreenComponent implements OnInit{
+  
+  public token: string = "";
+  
+  constructor(
+    private facadeService: FacadeService,
+    private router: Router
+  ) { }
 
+  ngOnInit(): void {
+    //Validar que haya inicio de sesión
+    //Obtengo el token del login
+    this.token = this.facadeService.getSessionToken();
+    console.log("Token: ", this.token);
+    
+    if(this.token == ""){
+      this.router.navigate([""]);
+    }   
+  }
+
+  //Cerrar Sesión
+  public logout(){
+    this.facadeService.logout().subscribe({
+      next: (response) => {
+        console.log("Entró");
+        
+        this.facadeService.destroyUser();
+        //Navega al login
+        this.router.navigate(["/"]);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
